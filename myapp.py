b3201e8d2ed303327db381485daea99363da7e29
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2025-05-04 12:09:46 +0900

    2025_05_04

--- myapp.py
+++ myapp.py
@@ -3,6 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return render_template("base.html", title="言葉を紡ぐ")
+@app.route("/<int:numbar>")
+def hello_world(numbar):
+    posts = [
+        {
+            "title": "記事のタイトル",
+            "body": "記事の内容",
+            "created_at": "2025-05-02",
+        },
+        {
+            "title": "記事のタイトル2",
+            "body": "記事の内容2",
+            "created_at": "2025-05-02",
+        },
+        {
+            "title": "記事のタイトル3",
+            "body": "記事の内容",
+            "created_at": "2025-05-02",
+        },
+    ]
+    post = posts[numbar]
+    return render_template("base.html", post=post)
